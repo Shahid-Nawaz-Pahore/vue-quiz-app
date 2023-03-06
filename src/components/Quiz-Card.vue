17de9460ea278b0cdf4b93b16c0e5f2477a4926a
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-07 02:09:35 +0500

    half

--- src/components/Quiz-Card.vue
+++ src/components/Quiz-Card.vue
@@ -1,33 +1,39 @@
+<!-- <script setup>
+
+import {defineProps}from 'vue'
+const {quiz} = defineProps(['quiz'])
+</script>
 <template>
-    <div class="card">
-        <img src="https://marvel-b1-cdn.bc0a.com/f00000000026007/resilienteducator.com/wp-content/uploads/2012/11/AdobeStock_60467600_cup.jpg" alt="">
-        <div class="card-text">
-            <h2>Math</h2>
-            <p>3 qustions</p>
-        </div>
+  <div class="card">
+    <img :src="quiz.img" alt="">
+    <div class="card-text">
+      <h2>{{ quiz.name }}</h2>
+      <p>{{ quiz.qustions.length }} qustions</p>
     </div>
+  </div>
 </template>
 <style scoped>
-.card{
-width: 310px;
-overflow: hidden;
-border-radius:2% ;
-box-shadow: 1px 1px 10px rgb(0, 0,0,0, 1);
-margin-bottom: 35px;
-margin-right: 20px;
-cursor: pointer;
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgb(0, 0, 0, 0, 1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
 }
-.card img{
- width: 100%;
- height: 190px;
- margin: 0;
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
 }
-.card .card-text{
+
+.card .card-text {
   padding: 0 5px;
 }
-.card .card-text h2{
+
+.card .card-text h2 {
   font-weight: bold;
 }
-
-
-</style>
+</style> -->
