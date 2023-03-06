49ad2879f54aa79ca693b431c1763f21edfed699
Shahid Nawaz <shahidnawaz.edu.pk@gmail.com>
2023-03-07 01:24:02 +0500

    add card as a component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from './data/quizes.json';
 import {ref , watch} from 'vue';
+import Card from './components/Quiz-Card.vue'
 const quizes = ref(q)
 const search = ref("")
 watch(search, ()=>{
@@ -16,13 +17,14 @@ quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.to
       <input v-model.trim="search" type="text" placeholder="Please search here...">
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.iq" class="card">
+      <Card/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.iq" class="card">
         <img  :src="quiz.img" a lt="subject image">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{quiz.questions.length}} Qustions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -54,26 +56,5 @@ border: none;
   margin-top: 40px;
 }
 /* card style */
-.card{
-width: 310px;
-overflow: hidden;
-border-radius:2% ;
-box-shadow: 1px 1px 10px rgb(0, 0,0,0, 1);
-margin-bottom: 35px;
-margin-right: 20px;
-cursor: pointer;
-}
-.card img{
- width: 100%;
- height: 190px;
- margin: 0;
-}
-.card .card-text{
-  padding: 0 5px;
-}
-.card .card-text h2{
-  font-weight: bold;
-}
-
 
 </style>
